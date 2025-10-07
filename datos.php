2464171be462edce526c49dddeba8226506bc06d
SkizoDev90 <franciscomanuelcarrillofernandez.guadalupe@alumnado.fundacionloyola.net>
2025-10-07 17:55:21 +0200

    FinalPROVPOO

--- datos.php
+++ datos.php
@@ -1,21 +1,6 @@
 <?php
     require_once "fecha.php";
     $fecha=$_GET["fecha"];
-    $meses = [
-        ["Enero", 31],
-        ["Febrero", 28],
-        ["Marzo", 31],
-        ["Abril", 30],
-        ["Mayo", 31],
-        ["Junio", 30],
-        ["Julio", 31],
-        ["Agosto", 31],
-        ["Septiembre", 30],
-        ["Octubre", 31],
-        ["Noviembre", 30],
-        ["Diciembre", 31]
-    ];
     $resultado=new Fecha($fecha);
-    $out=$resultado->mostrar($meses);
-    echo $out;
+    $resultado->mostrar();
 ?>
--- fecha.php
+++ fecha.php
@@ -4,24 +4,49 @@
         public function __construct($fecha) {
             $this->fecha = $fecha;
         }
-        public function mostrar($meses): string{
-            $arrayFecha = [];
+
+        public function mostrar(): void{
+            $meses = [
+                1=>["nombre"=>"Enero", "dias"=>31],
+                2=>["nombre"=>"Febrero","dias"=>28],
+                3=>["nombre"=>"Marzo","dias"=>31],
+                4=>["nombre"=>"Abril","dias"=>30],
+                5=>["nombre"=>"Mayo","dias"=>31],
+                6=>["nombre"=>"Junio","dias"=>30],
+                7=>["nombre"=>"Julio","dias"=>31],
+                8=>["nombre"=>"Agosto","dias"=>31],
+                9=>["nombre"=>"Septiembre","dias"=>30],
+                10=>["nombre"=>"Octubre","dias"=>31],
+                11=>["nombre"=>"Noviembre","dias"=>30],
+                12=>["nombre"=>"Diciembre","dias"=>31]
+                ];
+
             $arrayFecha=explode("-",$this->fecha);  //paso la fecha a un array
-            foreach ($meses as $indice=>$value) {
-                if($indice+1 == $arrayFecha[1]){                        //si el indice del array +1 coincide con el mes que se recoje.....
-                    $arrayFecha[1]=$value[0];                           //cargamos en el mes el string correspondiente
-                    if($this->comprobar($arrayFecha[0]) && $arrayFecha[1]=="Febrero"){          //pasamos a comprobar si es bisiesto y febrero
-                        $salida=implode("/",$arrayFecha);                           //volvemos el array un string
-                        return $salida . "<br>Es bisiesto y tiene " . ($value[1] + 1) . " dias";    
-                    }else if(!$this->comprobar($arrayFecha[0])){    //y si no es bisitesto....
-                        $salida=implode("/",$arrayFecha);
-                        return $salida." <br>No es bisiesto y tiene ".$value[1]." dias";
-                    };
+
+            if($this->comprobar($arrayFecha[0])){   //comprobamos si es bisiesto para saber si cambiar el array
+                $this->cambiarArray($meses);
+            }
+
+            foreach ($meses as $indice => $value) {
+                if ($indice == $arrayFecha[1]) {                                    // si el índice coincide con el mes...
+                    $arrayFecha[1] = $value["nombre"];                              // cambiamos el número por el nombre
+                    $salida = implode("/", $arrayFecha);          // volvemos el array un string
+                    
+                    if ($this->comprobar($arrayFecha[0]) && $value["nombre"] == "Febrero") {
+                        echo $salida . "<br> Es bisiesto y tiene " . $value["dias"] . " días";
+                    } else if ($this->comprobar($arrayFecha[0]) && $value["nombre"] != "Febrero") {
+                        echo $salida . "<br> Es bisiesto y este mes tiene " . $value["dias"] . " días" . " y ".$meses[2]["nombre"]." tiene ".$meses[2]["dias"]." dias";
+                    } else if(!$this->comprobar($arrayFecha[0])){
+                        echo $salida . "<br> No es bisiesto y este mes tiene " . $value["dias"] . " días" . " y ".$meses[2]["nombre"]." tiene ".$meses[2]["dias"]." dias";
+                    }
                 }
             }
         }
+        private function cambiarArray(&$meses){
+            $meses[2]["dias"]=29;
+        }
         private function comprobar($anio): bool{
-            if($anio%4 == 0 && $anio%100 != 0 || ($anio%400 == 0)){
+            if($anio%4 == 0 && $anio % 100 != 0 || ($anio % 400 == 0)){
                 return true;    //si es bisiesto devuelve true
             }else{
                 return false;   //sino es bisiesto....
